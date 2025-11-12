fdebb69cb7ed9f2e70e3e69a8e8cb010f1e76891
Simon <simonshaju@gmail.com>
2025-11-12 10:36:35 -0600

    Adding Javascript functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,11 @@ from os import environ
 from flask import Flask, render_template
 from flask_login import LoginManager
 from app.routes.user_management import init_user_management
+from app.routes.cart_api import cart_api_bp
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY", "secret")
 login = LoginManager(app)
 
-
-@app.route("/")
-@app.route("/index")
-def catalog():
-    return render_template("shop/index.html")
-
-
+app.register_blueprint(cart_api_bp)
 init_user_management(app, login)
