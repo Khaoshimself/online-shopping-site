851566db1851a153e5efd00d57701c0c0dd819af
Mark P <markpcollege@gmail.com>
2025-11-22 10:37:25 -0600

    added import for items_bp, and registered items_bp

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 from flask_login import LoginManager
 from app.routes.user_management import init_user_management
 from app.routes.cart_api import cart_api_bp
+from app.routes.items_admin import items_bp
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY", "secret")
 login = LoginManager(app)
 
 app.register_blueprint(cart_api_bp)
+app.register_blueprint(items_bp)
 init_user_management(app, login)
