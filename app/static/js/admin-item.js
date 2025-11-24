910d13146e39ed55d2da601aba0ba8ac28a9b82c
Mark P <markpcollege@gmail.com>
2025-11-24 15:38:04 -0600

    started work on function fetchAndDisplayItems(). also added new variables for a list container, a submit button, and an ID that indicates whether an item is currently in edit mode

--- app/static/js/admin-item.js
+++ app/static/js/admin-item.js
@@ -5,7 +5,40 @@ document.addEventListener('DOMContentLoaded', () =>
 {
     const form = document.getElementById('admin-item-form');
     const result = document.getElementById('admin-item-result');
+    const listContainer = document.getElementById('admin-items-list'); // fetch the list container from HTML -- will hold our items
+    const submitButton = document.getElementById('admin-item-submit');
+    let editId = null; // track if we are editing an item
     
+
+    async function fetchAndDisplayItems()
+    {
+        listContainer.textContent = 'Loading items...';
+        try
+        {
+            const resp = await fetch('/admin/items/list', {credentials : 'same-origin'});
+            const data = await resp.json();
+            if(!resp.ok) // check for a bad response
+            {
+                listContainer.innerHTML = `<div class="alert alert-danger">${data.error || 'Failed to load items'}</div>`;
+                return;
+            }
+            if(data.items.length === 0) // check if there are no items
+            {
+                listContainer.innerHTML = '<div class="alert alert-info">No items found.</div>';
+                return;
+            }
+            // build the items list
+            // dummy printing for now
+            listContainer.innerHTML = '';
+            // FUNCTION CALL HERE
+        }
+        catch(err)
+        {
+            listContainer.innerHTML = `<div class="alert alert-danger">Network error</div>`;
+            console.error(err);
+        }
+    }
+
     form.addEventListener('submit', async (ev) =>
     {
         ev.preventDefault();
