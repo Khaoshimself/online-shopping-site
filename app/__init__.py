ae81ae1d9f01ea3a2bf6a8d509bdff0da66ec7b7
Mark P <markpcollege@gmail.com>
2025-11-29 16:17:29 -0600

    added route initializations

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.routes.cart_api import cart_api_bp
 from app.routes.items_admin import items_bp
 from app.routes.users_admin import user_bp
 from app.routes.orders_admin import order_bp
+from app.routes.discounts_admin import discounts_bp
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY", "secret")
@@ -15,4 +16,5 @@ app.register_blueprint(cart_api_bp)
 app.register_blueprint(items_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(order_bp)
+app.register_blueprint(discounts_bp)
 init_user_management(app, login)
